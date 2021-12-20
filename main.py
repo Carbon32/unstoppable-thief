bc1661c9aeff6a51c442507e1a606ebda547a710
Carbon32 <contact.carbonofficial@gmail.com>
2021-12-20 11:28:48 +0100

    The game now can load levels

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ screenHeight = 1000
 
 gameWindow = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Unstoppable Thief: ")
+gameWindow.fill((100, 123, 255))
 
 # Frame Limiter: #
 
@@ -29,12 +30,72 @@ FPS = 60
 # Game Variables: #
 
 gameRunning = True
+tileSize = 50
+
+# Game Classes: #
+
+class World():
+	def __init__(self, data):
+		self.tileList = []
+		# Load Tile:
+		dirtTile = pygame.image.load('assets/Tiles/dirt.png')
+
+		# Select Tile:
+		rowCount = 0
+		for row in data:
+			columnCount = 0 
+			for tile in row:
+				if tile == 1:
+					image = pygame.transform.scale(dirtTile, (tileSize, tileSize))
+					imageRect = image.get_rect()
+					imageRect.x = columnCount * tileSize
+					imageRect.y = rowCount * tileSize
+					tile = (image, imageRect)
+					self.tileList.append(tile)
+				columnCount += 1
+			rowCount += 1
+
+	def draw(self):
+		for tile in self.tileList:
+			gameWindow.blit(tile[0], tile[1])
+
+
+# Game Mechanics: #
+
+worldData = [ # Test Level: 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+
+world = World(worldData)
 
 # Game Loop: #
 
 while(gameRunning):
 	# FPS Handler: 
 	handleFPS.tick(FPS)
+
+	# Handle Game Mechanics:
+	world.draw()
 
 	# Event Handler: 
 	for event in pygame.event.get():
@@ -43,5 +104,5 @@ while(gameRunning):
 
 	# Update Game Window: 
 	pygame.display.update()
-	
+
 pygame.quit()
